d1dbc05535be376f2ff5387e62b7f96693856e11
CatherineFu <catherine@modiface.com>
2018-02-13 11:56:25 -0500

    complete "Static"

---
+++ ExpressCourse/2_Static.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const app = express();
+
+app.use(express.static(process.argv[3]));
+
+app.listen(process.argv[2], function() {
+	console.log('Example app listening on port' +  process.argv[2]);
+});
+
+
+
+/*
+ Here's the official solution in case you want to compare notes:
+
+ ────────────────────────────────────────────────────────────────────────────────
+
+
+ var path = require('path')
+ var express = require('express')
+ var app = express()
+
+ app.use(express.static(process.argv[3]||path.join(__dirname, 'public')));
+
+ app.listen(process.argv[2])
+
+ ────────────────────────────────────────────────────────────────────────────────
+ */
